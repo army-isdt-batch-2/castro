6fd3b5d9502187c63a46f0fa72f4ed52cf2eb714
“CastroGenelyn” <genelync39@gmail.com>
2021-05-26 15:32:50 -0700

    Add the save routes with functionality to the DB

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Department;
+use Redirect;
 
 class DepartmentController extends Controller
 {
@@ -14,5 +16,23 @@ class DepartmentController extends Controller
     public function department_create()
     {
         return view('create_form.department');
+    }
+
+    //declare variable
+    protected $request;
+
+    //initiate class upon controller start    
+    public function __construct(Request $request)
+    {
+        //make the request available on the class
+        $this->request = $request;
+    }
+
+    public function department_create_save()
+    {
+        $data = $this->request->except('_token');
+        Department::create($data);
+
+        return Redirect::route('department.home');
     }
 }
