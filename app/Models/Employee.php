66ca5b88bf8ed6dc3219a7f46c8ff3e16bfeffdb
“CastroGenelyn” <genelync39@gmail.com>
2021-05-26 16:37:03 -0700

    Add Model for employees table

---
+++ app/Models/Employee.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Employee extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = "employees";
+
+    protected $fillable= [
+        'photo',
+        'fname',
+        'lname',
+        'mname',
+        'contact',
+        'address',
+        'birthday',
+        'gender',
+        'designation',
+        'rate',
+        'department_id',
+        'sss_id',
+        'ph_id',
+        'pi_id',
+        'tax_id',
+        'deleted_at',
+        'created_at',
+        'updated_at'
+  ];
+}
