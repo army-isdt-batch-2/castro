958c03be66717759828d75d8693ffbcfa81f0c75
“CastroGenelyn” <genelync39@gmail.com>
2021-05-26 15:24:37 -0700

    Name the Routes

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/login',[
+Route::get('/',[
     App\Http\Controllers\LoginController::class,
     'index'
 ]);
@@ -75,47 +71,87 @@ Route::get('/form',[
 Route::get('/department',[
     App\Http\Controllers\DepartmentController::class,
     'department'
-]);
+])->name('department.home');
 
 Route::get('/department/create',[
     App\Http\Controllers\DepartmentController::class,
     'department_create'
-]);
+])->name('department.create');
+
+Route::post('/department/create/save',[
+    App\Http\Controllers\DepartmentController::class,
+    'department_create_save'
+])->name('department.create.save');
 
 Route::get('/employees/create',[
     App\Http\Controllers\EmployeesController::class,
     'employees_create'
-]);
+])->name('employees.home');
+
+Route::post('/employees/create/save',[
+    App\Http\Controllers\EmployeesController::class,
+    'employees_create_save'
+])->name('employees.create.save');
 
 Route::get('/leaves/create',[
     App\Http\Controllers\LeavesController::class,
     'leaves_create'
-]);
+])->name('leaves.home');
+
+Route::post('/leaves/create/save',[
+    App\Http\Controllers\LeavesController::class,
+    'leaves_create_save'
+])->name('leaves.create.save');
 
 Route::get('/loans/create',[
     App\Http\Controllers\LoansController::class,
     'loans_create'
-]);
+])->name('loans.home');
+
+Route::post('/loans/create/save',[
+    App\Http\Controllers\LoansController::class,
+    'loans_create_save'
+])->name('loans.create.save');
 
 Route::get('/mandatory-deductions/create',[
     App\Http\Controllers\DeductionsController::class,
     'mandatory_deductions_create'
-]);
+])->name('mandatory_deductions.home');
+
+Route::post('/mandatory-deductions/create/save',[
+    App\Http\Controllers\DeductionsController::class,
+    'mandatory_deductions_create_save'
+])->name('mandatory_deductions.create.save');
 
 Route::get('/timekeeping/create',[
     App\Http\Controllers\TimekeepingController::class,
     'timekeeping_create'
-]);
+])->name('timekeeping.home');
+
+Route::post('/timekeeping/create/save',[
+    App\Http\Controllers\TimekeepingController::class,
+    'timekeeping_create_save'
+])->name('timekeeping.create.save');
 
 Route::get('/payslips/create',[
     App\Http\Controllers\PayslipsController::class,
     'payslips_create'
-]);
+])->name('payslips.home');
+
+Route::post('/payslips/create/save',[
+    App\Http\Controllers\PayslipsController::class,
+    'payslips_create_save'
+])->name('payslips.create.save');
 
 Route::get('/settings/create',[
     App\Http\Controllers\SettingsController::class,
     'settings_create'
-]);
+])->name('settings.home');
+
+Route::post('/settings/create/save',[
+    App\Http\Controllers\SettingsController::class,
+    'settings_create_save'
+])->name('settings.create.save');
 
 Route::get('/timekeeping/view',[
     App\Http\Controllers\TimekeepingController::class,
